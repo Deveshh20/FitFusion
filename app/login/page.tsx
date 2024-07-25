1529b522a246e14319dc760a5e747cbc4489113c
Devesh <24421825+Deveshh20@users.noreply.github.com>
2024-07-25 21:27:01 +0530

    Fix unescaped characters in login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -25,26 +25,26 @@ export default function LoginPage() {
             const response = await axios.post("/api/users/login", user);
             console.log("Login Success", response.data);
 
-            const { role, username,profileCompleted} = response.data;
-             // Replace spaces with hyphens in the username
+            const { role, username, profileCompleted } = response.data;
+            // Replace spaces with hyphens in the username
             //  const formattedUsername = username.replace(/\s+/g, '-');
 
             // Redirect based on the user's role
             if (role === 'trainer') {
-                if(profileCompleted){
-                router.push(`/profile/trainer-profile/${username}`);
+                if (profileCompleted) {
+                    router.push(`/profile/trainer-profile/${username}`);
                 }
-                else{
-                router.push(`/profile/trainer-dashboard/${username}`); // Use dynamic route for trainer    
+                else {
+                    router.push(`/profile/trainer-dashboard/${username}`); // Use dynamic route for trainer    
                 }
-                
+
             } else if (role === 'trainee') {
-                if(profileCompleted){
+                if (profileCompleted) {
                     router.push(`/profile/trainee-profile/${username}`);
-                    }
-                    else{
+                }
+                else {
                     router.push(`/profile/trainee-dashboard/${username}`); // Use dynamic route for trainer    
-                    }
+                }
             }
 
         } catch (error: any) {
@@ -93,7 +93,9 @@ export default function LoginPage() {
                     <div className="flex items-start">
                         <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required />
                         <div className="ml-3 text-sm">
-                            <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Terms and Conditions</a></label>
+                            <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">
+                                I accept the <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Terms and Conditions</a>
+                            </label>
                         </div>
                     </div>
 
